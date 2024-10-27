19e269120d752d5c5a288941eac07ee4ab7c41c3
Indrani Sarkar <84556248+Ira10@users.noreply.github.com>
2024-10-27 16:37:31 +0530

    Create Find start and destination for each c_id.sql

---
+++ Find start and destination for each c_id.sql
@@ -0,0 +1,8 @@
+SELECT f1.cid, f1.origin, f2.Destination	
+	
+from flights f1	
+JOIN flights f2	
+ON f1.cid = f2.cid AND f1.Destination = f2.origin;	
+
+
+https://docs.google.com/spreadsheets/d/1_Y46EL4nvmdzsGFqQLecMkhBNPWKt2rL-Neto4NU6Ek/edit?gid=1388912625#gid=1388912625
