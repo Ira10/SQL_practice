0bc667acda5b0552be72b6c91982ab201126e7a8
Indrani Sarkar <84556248+Ira10@users.noreply.github.com>
2026-07-06 21:56:07 +0530

    Remove empty line in Company Revenue.sql

--- Company Revenue.sql
+++ Company Revenue.sql
@@ -1,6 +1,6 @@
 https://docs.google.com/spreadsheets/d/1_Y46EL4nvmdzsGFqQLecMkhBNPWKt2rL-Neto4NU6Ek/edit?gid=1522020406#gid=1522020406
 
-
+ 
 WITH base AS (	
 SELECT *,	
 COALESCE(lag(year) over(partition by company order by year),0) as last_year,	
